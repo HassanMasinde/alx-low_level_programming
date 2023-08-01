c8b9d388305f8afb72c421d0fea7bed2c0dac528
HassanMasinde <wesongahassan15@gmail.com>
2023-08-01 11:53:34 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,14 +1,17 @@
 #include "lists.h"
 
-size_t print_listint(const listint_t *h) {
-    size_t node_count = 0;
+size_t print_listint(const listint_t *h)
+{
 
-    while (h != NULL) {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
+size_t node_count = 0;
 
-    return node_count;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+node_count++;
+}
+
+return (node_count);
 }
 
