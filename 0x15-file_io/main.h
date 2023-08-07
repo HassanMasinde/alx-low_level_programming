b92aeaae88fc7bebf0a537c6cd60fe6628041e72
HassanMasinde <wesongahassan15@gmail.com>
2023-08-07 11:02:46 -0700

    a

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/* a function that reads a text file and prints it to the POSIX standard output */
+/* a function that reads a text file and prints it */
 ssize_t read_textfile(const char *filename, size_t letters);
 /* a function that creates a file.*/
 int create_file(const char *filename, char *text_content);
