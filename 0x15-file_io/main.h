95cb221d6040a565f16f9a8e2ddb15da3a550c44
HassanMasinde <wesongahassan15@gmail.com>
2023-08-07 12:11:46 -0700

    d

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content);
 /* a function that appends text at the end of a file.*/
 
 int append_text_to_file(const char *filename, char *text_content);
-int cp(const char *file_from, const char *file_to);
+int copy_file(const char *src_filename, const char *dest_filename);
 int _putchar(char c);
 
 #endif
